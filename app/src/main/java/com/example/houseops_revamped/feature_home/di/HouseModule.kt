02470ffb2ddcfda68d3a84b5e5bc71a0abe552b8
KenStarry <starrycodes@gmail.com>
2023-01-24 21:19:19 +0300

    Home repository module

--- app/src/main/java/com/example/houseops_revamped/feature_home/di/HouseModule.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_home/di/HouseModule.kt
@@ -1,12 +1,22 @@
 package com.example.houseops_revamped.feature_home.di
 
+import com.example.houseops_revamped.feature_home.data.repository.HomeRepositoryImpl
+import com.example.houseops_revamped.feature_home.domain.repository.HomeRepository
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object HouseModule {
 
     //  repository
+    @Provides
+    @Singleton
+    fun provideHomeRepository(
+        db: FirebaseFirestore
+    ) : HomeRepository = HomeRepositoryImpl(db)
 }
