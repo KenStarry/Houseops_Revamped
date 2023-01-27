754e5477ab66e984ad1721b69629b2171888dfea
KenStarry <starrycodes@gmail.com>
2023-01-27 23:02:39 +0300

    Bookmark functionality

--- app/src/main/java/com/example/houseops_revamped/feature_home/presentation/utils/HomeConstants.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_home/presentation/utils/HomeConstants.kt
@@ -7,12 +7,12 @@ import com.example.houseops_revamped.feature_home.presentation.models.PillBtn
 object HomeConstants {
 
     val homePills = listOf(
-        PillBtn(Icons.Outlined.Apartment, "For Rent"),
-        PillBtn(Icons.Outlined.Person3, "Caretakers"),
+        PillBtn(Icons.Outlined.Key, "For Rent"),
+        PillBtn(Icons.Outlined.Apartment, "Apartments"),
         PillBtn(Icons.Outlined.Paid, "For Sale"),
-        PillBtn(Icons.Outlined.Hotel, "One Bedroom"),
+        PillBtn(Icons.Outlined.LocationOn, "Near You"),
         PillBtn(Icons.Outlined.Key, "Mansions"),
-        PillBtn(Icons.Outlined.Apartment, "Two Bedroom"),
+        PillBtn(Icons.Outlined.Person3, "Caretakers"),
         PillBtn(Icons.Outlined.Apartment, "Three Bedroom")
     )
 }
