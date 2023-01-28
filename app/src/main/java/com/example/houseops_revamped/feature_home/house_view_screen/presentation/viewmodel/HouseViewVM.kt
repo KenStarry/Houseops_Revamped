cae0123a38b68ef1cdb5da3870414edc46272495
KenStarry <starrycodes@gmail.com>
2023-01-28 15:17:56 +0300

    House View viewmodel

--- app/src/main/java/com/example/houseops_revamped/feature_home/house_view_screen/presentation/viewmodel/HouseViewVM.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_home/house_view_screen/presentation/viewmodel/HouseViewVM.kt
@@ -1,14 +1,36 @@
 package com.example.houseops_revamped.feature_home.house_view_screen.presentation.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.houseops_revamped.feature_home.home_screen.domain.model.HouseModel
 import com.example.houseops_revamped.feature_home.house_view_screen.domain.use_case.HouseViewUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HouseViewVM @Inject constructor(
-    private val houseViewUseCases: HouseViewUseCases
+    private val useCase: HouseViewUseCases
 ) : ViewModel() {
 
+    var currentHouse by mutableStateOf<HouseModel?>(null)
 
+    operator fun invoke(
+        apartment: String,
+        category: String
+    ) {
+        viewModelScope.launch {
+
+            useCase.getHouse(
+                apartmentName = apartment,
+                category = category,
+                currentHouse = {
+                    currentHouse = it
+                }
+            )
+        }
+    }
 }
