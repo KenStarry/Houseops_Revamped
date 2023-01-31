8ef47a373ab87c5bd8dbfe22f68df3510e514643
KenStarry <starrycodes@gmail.com>
2023-01-31 12:58:40 +0300

    Bookmarks app bar

--- app/src/main/java/com/example/houseops_revamped/feature_bookmark/presentation/components/BookmarksAppBar.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_bookmark/presentation/components/BookmarksAppBar.kt
@@ -1,10 +1,16 @@
 package com.example.houseops_revamped.feature_bookmark.presentation.components
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.ArrowRight
+import androidx.compose.material.icons.outlined.Sort
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -14,11 +20,58 @@ fun BookmarksAppBar(
 
     LargeTopAppBar(
         title = {
-            Text(
-                text = "Bookmarks",
-                fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                fontWeight = FontWeight.ExtraBold
-            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .padding(end = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Text(
+                    text = "Bookmarks",
+                    fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                    fontWeight = FontWeight.ExtraBold,
+                    modifier = Modifier
+                        .weight(5f)
+                )
+
+                //  sort button
+                Row(
+                    modifier = Modifier
+                        .weight(2f),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Text(
+                        text = "Sort",
+                        fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowRight,
+                        contentDescription = "Arrow icon"
+                    )
+
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        colors = IconButtonDefaults.iconButtonColors(
+                            containerColor = MaterialTheme.colorScheme.tertiary
+                        )
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Sort,
+                            contentDescription = "Sort icon",
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                    }
+
+                }
+
+            }
         },
         navigationIcon = {
             IconButton(onClick = { onBackPressed() }) {
