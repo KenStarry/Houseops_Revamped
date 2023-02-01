c3b7f3ad568395ed07e5f2c2b7b762f545baf517
KenStarry <starrycodes@gmail.com>
2023-02-01 21:48:29 +0300

    Fixed duplicate class issue

--- app/src/main/java/com/example/houseops_revamped/feature_bookmark/presentation/components/BookmarkCategories.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_bookmark/presentation/components/BookmarkCategories.kt
@@ -1,24 +1,23 @@
 package com.example.houseops_revamped.feature_bookmark.presentation.components
 
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.houseops_revamped.R
 import com.example.houseops_revamped.core.domain.model.HouseCategoryModel
 import com.example.houseops_revamped.core.domain.model.UsersCollection
 import com.example.houseops_revamped.feature_bookmark.domain.model.BookmarkEvents
@@ -29,7 +28,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.HorizontalPagerIndicator
 import com.google.accompanist.pager.rememberPagerState
 
-@OptIn(ExperimentalAnimationApi::class, ExperimentalPagerApi::class)
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BookmarkCategories(
     bookmarksVM: BookmarksViewModel,
@@ -49,37 +48,68 @@ fun BookmarkCategories(
         )
     )
 
+    Toast.makeText(
+        LocalContext.current,
+        bookmarksVM.listOfCategories.value.size.toString(),
+        Toast.LENGTH_SHORT
+    ).show()
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
 
-        HorizontalPagerIndicator(
-            pagerState = pagerState,
-            activeColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f),
-            inactiveColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.1f),
-            indicatorHeight = 5.dp,
-            indicatorWidth = 5.dp,
-            indicatorShape = CircleShape,
-            spacing = 4.dp,
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        )
+        //  horizontal pager and indicators
+        AnimatedVisibility(
+            visible = bookmarksVM.listOfCategories.value.isNotEmpty()
+        ) {
+            HorizontalPagerIndicator(
+                pagerState = pagerState,
+                activeColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f),
+                inactiveColor = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.1f),
+                indicatorHeight = 5.dp,
+                indicatorWidth = 5.dp,
+                indicatorShape = CircleShape,
+                spacing = 4.dp,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
 
-        HorizontalPager(
-            count = bookmarksVM.listOfCategories.value.size,
-            state = pagerState,
-            itemSpacing = 16.dp
-        ) { page ->
-            //  category item
-            CategoryItem(
-                context = context,
-                navHostController = navHostController,
-                houseCategory = bookmarksVM.listOfCategories.value[page],
-                bookmarkedHouses = bookmarkedHouses,
-                currentUser = currentUser,
-                snackbarHostState = snackbarHostState
+            HorizontalPager(
+                count = bookmarksVM.listOfCategories.value.size,
+                state = pagerState,
+                itemSpacing = 16.dp
+            ) { page ->
+                //  category item
+                CategoryItem(
+                    context = context,
+                    navHostController = navHostController,
+                    houseCategory = bookmarksVM.listOfCategories.value[page],
+                    bookmarkedHouses = bookmarkedHouses,
+                    currentUser = currentUser,
+                    snackbarHostState = snackbarHostState
+                )
+            }
+        }
+
+        //  no bookmarks svg
+        AnimatedVisibility(visible = bookmarksVM.listOfCategories.value.isEmpty()) {
+
+            val composition by rememberLottieComposition(
+                LottieCompositionSpec
+                    .RawRes(R.raw.success_lottie)
+            )
+
+            val progress by animateLottieCompositionAsState(
+                composition = composition
+            )
+
+            LottieAnimation(
+                composition = composition,
+                progress = progress,
+                modifier = Modifier
+                    .fillMaxSize()
             )
         }
     }
