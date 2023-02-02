f54a88fb8add59c394e829db3ce12e4b2c85cf72
KenStarry <starrycodes@gmail.com>
2023-02-02 18:12:11 +0300

    Caretaker section

--- app/src/main/java/com/example/houseops_revamped/feature_home/home_screen/presentation/utils/HomeConstants.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_home/home_screen/presentation/utils/HomeConstants.kt
@@ -7,11 +7,11 @@ import com.example.houseops_revamped.feature_home.home_screen.presentation.model
 object HomeConstants {
 
     val homePills = listOf(
+        PillBtn(Icons.Outlined.Apartment, "House Categories"),
         PillBtn(Icons.Outlined.Key, "For Rent"),
         PillBtn(Icons.Outlined.Apartment, "Apartments"),
         PillBtn(Icons.Outlined.Paid, "For Sale"),
         PillBtn(Icons.Outlined.LocationOn, "Near You"),
         PillBtn(Icons.Outlined.Person3, "Caretakers"),
-        PillBtn(Icons.Outlined.Apartment, "House Categories")
     )
 }
