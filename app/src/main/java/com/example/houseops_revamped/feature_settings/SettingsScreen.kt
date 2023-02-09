de3657a8d79b43d6544c98b3e5b4c6d6af354543
KenStarry <starrycodes@gmail.com>
2023-02-09 10:26:36 +0300

    Optimized imports

--- app/src/main/java/com/example/houseops_revamped/feature_settings/SettingsScreen.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_settings/SettingsScreen.kt
@@ -1,42 +1,18 @@
 package com.example.houseops_revamped.feature_settings
 
-import android.content.Context
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.sharp.ChevronRight
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
-import coil.request.ImageRequest
-import com.example.houseops_revamped.R
-import com.example.houseops_revamped.custom_components.SettingsTopAppBar
-import com.example.houseops_revamped.core.domain.model.UsersCollection
 import com.example.houseops_revamped.feature_settings.presentation.components.SettingsAppBar
 import com.example.houseops_revamped.feature_settings.presentation.components.about_section.AboutSection
 import com.example.houseops_revamped.feature_settings.presentation.components.personalization_section.PersonalizationSection
 import com.example.houseops_revamped.feature_settings.presentation.components.themes_section.ThemesSection
 import com.example.houseops_revamped.feature_settings.presentation.viewmodel.SettingsViewModel
-import com.example.houseops_revamped.navigation.Direction
-import com.example.houseops_revamped.ui.theme.HouseOps_RevampedTheme
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
