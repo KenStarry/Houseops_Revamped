61f398ac7b2e83e6ead5e636a38398548374f903
KenStarry <starrycodes@gmail.com>
2023-02-21 16:35:42 +0300

    Signup use cases

---
+++ app/src/main/java/com/example/houseops_revamped/feature_authentication/sign_up/domain/use_cases/SignUpUseCases.kt
@@ -0,0 +1,6 @@
+package com.example.houseops_revamped.feature_authentication.sign_up.domain.use_cases
+
+data class SignUpUseCases(
+    val createAccount: CreateAccount,
+    val createUserCollection: CreateUserCollection
+)
