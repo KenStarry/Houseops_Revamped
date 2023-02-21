0c0ee3ec9ba35ea540140160c8a4394857791f57
KenStarry <starrycodes@gmail.com>
2023-02-21 16:32:42 +0300

    Signup repository

--- app/src/main/java/com/example/houseops_revamped/feature_authentication/sign_up/data/repository/SignUpRepositoryImpl.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_authentication/sign_up/data/repository/SignUpRepositoryImpl.kt
@@ -2,7 +2,9 @@ package com.example.houseops_revamped.feature_authentication.sign_up.data.reposi
 
 import com.example.houseops_revamped.core.domain.model.Response
 import com.example.houseops_revamped.core.domain.model.UsersCollection
+import com.example.houseops_revamped.core.utils.Constants
 import com.example.houseops_revamped.feature_authentication.sign_up.domain.repository.SignUpRepository
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import javax.inject.Inject
@@ -17,15 +19,30 @@ class SignUpRepositoryImpl @Inject constructor(
         password: String,
         response: (response: Response) -> Unit
     ) {
-
-
+        try {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener { Response.Success }
+                .addOnFailureListener { Response.Failure(it) }
+        } catch (e: Exception) {
+            Response.Failure(e)
+        }
     }
 
     override suspend fun createUserCollection(
         user: UsersCollection,
         response: (response: Response) -> Unit
     ) {
-        TODO("Not yet implemented")
+        try {
+            user.userEmail?.let { email ->
+                db.collection(Constants.USERS_COLLECTION)
+                    .document(email)
+                    .set(user)
+                    .addOnSuccessListener { Response.Success }
+                    .addOnFailureListener { Response.Failure(it) }
+            }
+        } catch (e: FirebaseException) {
+            Response.Failure(e)
+        }
     }
 }
 
