1986ca85d4d3020693a3a08895b00c90038cda4f
KenStarry <starrycodes@gmail.com>
2023-03-03 20:51:06 +0300

    Admin Main Screen Redesigned

--- app/src/main/java/com/example/houseops_revamped/feature_admin/presentation/AdminMainScreen.kt
+++ app/src/main/java/com/example/houseops_revamped/feature_admin/presentation/AdminMainScreen.kt
@@ -1,14 +1,47 @@
 package com.example.houseops_revamped.feature_admin.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AdminMainScreen(
     navHostController: NavHostController
 ) {
 
-    Text(text = "Hello Admin!")
+    Scaffold(
+
+    ) { contentPadding ->
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(MaterialTheme.colorScheme.onPrimary)
+                .padding(contentPadding)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.onPrimary)
+                    .padding(16.dp)
+            ) {
+
+            }
+
+        }
+
+    }
 
 }
