c0f9fa15a692a0d770647448846d935c6eaf4d10
KenStarry <starrycodes@gmail.com>
2023-03-03 18:08:38 +0300

    Added admin and agent screens

---
+++ app/src/main/java/com/example/houseops_revamped/feature_admin/presentation/AdminScreen.kt
@@ -0,0 +1,9 @@
+package com.example.houseops_revamped.feature_admin.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AdminScreen() {
+
+
+}
---
+++ app/src/main/java/com/example/houseops_revamped/feature_agent/presentation/AgentScreen.kt
@@ -0,0 +1,8 @@
+package com.example.houseops_revamped.feature_agent.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AgentScreen() {
+
+}
