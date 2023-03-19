f03a21458719597db8364e9c20783506f93b5df5
KenStarry <starrycodes@gmail.com>
2023-03-19 16:52:54 +0300

    Redesigned places API

--- app/src/main/java/com/kenstarry/houseops_revamped/feature_landlord/feature_home/feature_add_apartment/presentation/components/bottomsheets/PlacesBottomSheet.kt
+++ app/src/main/java/com/kenstarry/houseops_revamped/feature_landlord/feature_home/feature_add_apartment/presentation/components/bottomsheets/PlacesBottomSheet.kt
@@ -1,5 +1,6 @@
 package com.kenstarry.houseops_revamped.feature_landlord.feature_home.feature_add_apartment.presentation.components.bottomsheets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +11,7 @@ import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
@@ -17,8 +19,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.kenstarry.houseops_revamped.R
 import com.kenstarry.houseops_revamped.feature_landlord.feature_home.feature_add_apartment.presentation.viewmodel.LndAddApartmentViewModel
 import com.kenstarry.houseops_revamped.core.domain.model.PlacesAPIResult
+import com.kenstarry.houseops_revamped.core.presentation.components.Lottie
 import com.kenstarry.houseops_revamped.feature_authentication.presentation.login.presentation.components.CustomTextField
 import com.kenstarry.houseops_revamped.feature_landlord.feature_home.feature_add_apartment.domain.model.LndApartmentEvents
 
@@ -59,38 +63,73 @@ fun PlacesBottomSheet(
             }
         )
 
-        LazyColumn(
-            content = {
-                items(
-                    items = lndAddApartmentVM.locationAutoFill
-                ) {
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp)
-                            .clickable {
-                                
-                                keyboardController?.hide()
-                                textLocation = it.address
-                                lndAddApartmentVM.locationAutoFill.clear()
-                                onInput(it)
-                            }
+        AnimatedVisibility(visible = lndAddApartmentVM.locationAutoFill.isNotEmpty()) {
+            LazyColumn(
+                content = {
+                    items(
+                        items = lndAddApartmentVM.locationAutoFill
                     ) {
 
-                        Text(
-                            text = it.address,
-                            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
-                            fontWeight = FontWeight.Bold
-                        )
-                        
-                        Spacer(modifier = Modifier.height(16.dp))
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .clickable {
 
+                                    keyboardController?.hide()
+                                    textLocation = it.address
+                                    lndAddApartmentVM.locationAutoFill.clear()
+                                    onInput(it)
+                                }
+                        ) {
+
+                            Text(
+                                text = it.address,
+                                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                                fontWeight = FontWeight.Bold
+                            )
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                        }
                     }
-                }
-            },
-            state = listState
-        )
+                },
+                state = listState,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
+
+        AnimatedVisibility(visible = lndAddApartmentVM.locationAutoFill.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Lottie(
+                    rawFile = R.raw.search_icon,
+                    isPlaying = true,
+                    iterations = Int.MAX_VALUE,
+                    modifier = Modifier
+                        .fillMaxSize(0.7f)
+                )
+                
+                Spacer(modifier = Modifier.height(24.dp))
+
+                Text(
+                    text = if (textLocation.isBlank())
+                        "Where is your apartment located?"
+                    else
+                        "Oops, could not find any results",
+                    fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                    fontWeight = FontWeight.SemiBold,
+                    color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
+                )
+
+            }
+        }
 
     }
 }
