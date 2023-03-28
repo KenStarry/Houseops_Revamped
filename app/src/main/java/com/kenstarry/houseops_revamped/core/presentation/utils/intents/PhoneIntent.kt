2aec5e99e1109f27a84a3182734c1ced9d8bff12
KenStarry <starrycodes@gmail.com>
2023-03-28 20:38:20 +0300

    added phone dial intent

--- app/src/main/java/com/kenstarry/houseops_revamped/core/presentation/utils/intents/PhoneIntent.kt
+++ app/src/main/java/com/kenstarry/houseops_revamped/core/presentation/utils/intents/PhoneIntent.kt
@@ -22,22 +22,3 @@ fun phoneDialIntent(
     }
 
 }
-
-fun phoneCallIntent(
-    context: Context,
-    phoneNumber: String
-) {
-
-    val parsePhone = Uri.parse("tel:$phoneNumber")
-    val intent = Intent(Intent.ACTION_CALL)
-
-    //  open intent
-    try {
-        intent.data = parsePhone
-        context.startActivity(intent)
-
-    } catch (e: SecurityException) {
-        Toast.makeText(context, "Error: $e", Toast.LENGTH_SHORT).show()
-    }
-
-}
